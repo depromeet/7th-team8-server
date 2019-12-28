13221b683e7ce0e4ee7626e968f72add6efd9fe6
junbly <junhyeon47@gmail.com>
2019-12-28 15:48:18 +0900

    initial commit

---
+++ src/main/java/com/nonoplan/NonoPlanApplication.java
@@ -0,0 +1,13 @@
+package com.nonoplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonoPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonoPlanApplication.class, args);
+	}
+
+}
